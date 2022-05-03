9bd75abd0ade8ce6ff04e23ce4cf7c079702b539
tjmarks37 <tjmarks@usc.edu>
2022-05-03 16:51:22 -0700

    Create clock_api.py

---
+++ clock_api.py
@@ -0,0 +1,44 @@
+import requests
+import socket
+import json
+
+
+
+def clock_init():
+    
+
+    response = requests.get('https://www.timeapi.io/api/Time/current/zone?timeZone=America/Los_Angeles')
+
+    if response.status_code == 200: # Status: OK
+        data = response.json()
+
+        # TODO: Extract the time from the data
+        hour=data['hour']
+        minute=data['minute']
+        seconds=data['seconds']
+        
+        time={
+        	'hour':hour,
+        	'minute':minute,
+        	'seconds':seconds
+        	}
+       
+        
+        
+        
+        return time
+
+    else:
+        print('error: got response code %d' % response.status_code)
+        print(response.text)
+        return None
+
+
+CLOCK_APP = {
+    'name': 'Time in Los Angeles',
+    'init': clock_init
+}
+
+
+if __name__ == '__main__':
+    clock_init()
