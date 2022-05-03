413d0d9c1f506e90a05c485eeb3efbb54ddefe88
tjmarks37 <tjmarks@usc.edu>
2022-05-03 16:52:16 -0700

    Create push_api.py

---
+++ push_api.py
@@ -0,0 +1,39 @@
+import requests
+import socket
+import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+
+def push_init():
+    
+
+    url = 'https://www.pushsafer.com/api'
+    post_fields = {
+	"t" : 'Alarm',
+	"m" : 'Alarm going off',
+	"s" : 11,
+	"v" : 3,
+	"i" : 33,
+	"c" : '#FF0000',
+	"d" : 'a',
+	"u" : 'https://www.pushsafer.com',
+	"ut" : 'Open Pushsafer',
+	"k" : '0S3YoMceEKWtCnneL9qM',
+	}
+
+    request = Request(url, urlencode(post_fields).encode())
+    json = urlopen(request).read().decode()
+    print(json)
+
+    
+
+
+PUSH_APP = {
+    'name': 'PUSH',
+    'init': push_init
+}
+
+
+if __name__ == '__main__':
+    push_init()
