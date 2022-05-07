876e50fe9c602e90ef25508462839314ae2dccc5
tjmarks37 <tjmarks@usc.edu>
2022-05-06 22:24:27 -0700

    herllo

--- rpi_clock.py
+++ rpi_clock.py
@@ -88,17 +88,17 @@ if __name__ == '__main__':
         	
         client.publish("tom_rohan/time", current_time)
         
-        if alarm_time==current_time:  
-           print("Alarm going off")
-           client.publish("tom_rohan/alarm_status","Alarm going off")
-           flag=1
+        #if alarm_time==current_time:  
+           #print("Alarm going off")
+           #client.publish("tom_rohan/alarm_status","Alarm going off")
+           #flag=1
            #alarm_time='off'
            #client.publish("tom_rohan/alarm", "off")
         #elif flag==1:
            #print("Alarm going off")
            #client.publish("tom_rohan/alarm_status","Alarm going off")
         
-        if grovepi.digitalRead(BTTN)=="1" and flag==1:
+        if grovepi.digitalRead(BTTN)=="1" :
            print("Alarm off!")
            client.publish("tom_rohan/alarm_status","Alarm turned off")
            alarm_time="off"
